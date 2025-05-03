480030211cb7babf76f4971b64a7fae35b814606
VladMakarenko99 <goze23gooz@gmail.com>
2025-05-03 15:51:52 +0200

    Added guards protection for routes

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -3,9 +3,10 @@ import {SignUpComponent} from './components/sign-up/sign-up.component';
 import {SignInComponent} from './components/sign-in/sign-in.component';
 import {AppComponent} from './app.component';
 import {HomeComponent} from './components/home/home.component';
+import {authLoginGuard} from './guards/auth-login.guard';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'sign-up', component: SignUpComponent },
-  { path: 'sign-in', component: SignInComponent },
+  { path: 'sign-up', component: SignUpComponent, canActivate: [authLoginGuard] },
+  { path: 'sign-in', component: SignInComponent, canActivate: [authLoginGuard] },
 ];
