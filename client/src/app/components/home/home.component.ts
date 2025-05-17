ffae8f570459fffa32f143384ac0667809552d66
VladMakarenko99 <goze23gooz@gmail.com>
2025-05-17 20:30:12 +0200

    Created internal calnder UI

--- client/src/app/components/home/home.component.ts
+++ client/src/app/components/home/home.component.ts
@@ -1,17 +1,17 @@
 import {Component, inject} from '@angular/core';
 import {AuthService} from '../../services/auth.service';
-import {MatToolbar, MatToolbarModule} from '@angular/material/toolbar';
-import {MatButtonModule} from '@angular/material/button';
 import {AuthOptionsComponent} from '../auth-options/auth-options.component';
 import {TodoListComponent} from '../todo-list/todo-list.component';
 import {HeaderComponent} from '../header/header.component';
+import {TodoCalendarComponent} from '../todo-calendar/todo-calendar.component';
 
 @Component({
   selector: 'app-home',
   imports: [
     AuthOptionsComponent,
     TodoListComponent,
-    HeaderComponent
+    HeaderComponent,
+    TodoCalendarComponent
   ],
   templateUrl: './home.component.html',
   standalone: true,
